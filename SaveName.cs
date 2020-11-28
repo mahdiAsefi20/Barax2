3e84876c589af79fcf662efe844db96248fb67bf
mahdiAsefi <mahdiasefi1379@gmail.com>
2020-11-28 23:01:35 +0330

    Add project files.

---
+++ SaveName.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveName : MonoBehaviour
+{
+ public Text name1;
+ public Text name2;
+
+   private void OnDisable()      
+   {
+       if(name1.text==null)
+       {
+           PlayerPrefs.SetString("name1", name1.text);
+       }
+
+   }
+}
---
+++ SceneController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    private Scene scene;
+    public void Start()
+    {
+     scene = SceneManager.GetActiveScene();
+     
+    }
+    private void Update()
+    {
+        if (scene.name.Equals("FristScene"))
+        { 
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+            } 
+        }
+    }
+    public void OnClick(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    
+}
